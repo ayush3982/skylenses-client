755167690e40f25bd4035eed618de5d8a42bffb8
ayush3982 <59345337+ayush3982@users.noreply.github.com>
2021-06-29 04:11:12 +0530

    Card

---
+++ src/components/cards/ProductCard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductCard = ({product}) => {
+    return (
+        <div>{product.title}</div>
+    )
+}
+
+export default ProductCard
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { getProducts} from "../functions/product";
+import ProductCard from '../components/cards/ProductCard';
 
 const Home = () => {
+
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading ] = useState(false)
+
+  const loadAllProducts = () => {
+    setLoading(true);
+    getProducts()
+      .then((res) => {
+        setProducts(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setLoading(false);
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    loadAllProducts();
+  }, [])
+
   return (
-    <div>
-      <p>React Home</p>
-    </div>
+    <>
+      <div className="jumbotron">
+        {loading ? (<h1 className="text-danger h1">Loading...</h1>) : (<h1>All Products</h1>)}
+      </div>
+      <div className="container">
+        <div className="row">
+          {products.map((product) => (
+            <div key = {product._id} className="col-md-4">
+              <ProductCard product={product} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 
